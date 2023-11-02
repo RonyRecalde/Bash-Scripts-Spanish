ce3e6b84e9d9d80428c5afd26008ce23bc3f1f0f
Carlos Rony Recalde <49035642+RonyRecalde@users.noreply.github.com>
2023-11-02 12:07:00 -0300

    Update interactuando-con-variables.sh

--- Scripts para Iniciantes/interactuando-con-variables.sh
+++ Scripts para Iniciantes/interactuando-con-variables.sh
@@ -11,3 +11,10 @@ echo -n " Hola, $nombre. Por favor, ingrese su apellido: ";read apellido
 #Visualizando el contenido asignado de las variables creadas
 echo "Hola $nombre $apellido. Bienvenido!"
 
+#Otra forma de hacerlo
+
+#El comando read lee las informaciones ingresadas y las guarda en las variables
+#La opción -p junto al comando read no crea una nueva línea
+read -p "Ingrese su nombre: " nombre
+read -p "Hola, $nombre. Por favor, ingrese su apellido: " apellido
+echo "Hola, $nombre $ apellido. Bienvenido!"
