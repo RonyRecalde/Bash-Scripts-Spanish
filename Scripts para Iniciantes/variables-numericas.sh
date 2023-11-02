2a5f18e8d7cb98589432fe4d2f221fdd67ad25ac
Carlos Rony Recalde <49035642+RonyRecalde@users.noreply.github.com>
2023-11-02 12:30:26 -0300

    Create variables-numericas.sh

---
+++ Scripts para Iniciantes/variables-numericas.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+clear
+
+#Las variables númericas no necesitan usar " " 
+numero=2023
+numero_1=2022
+read -p "Ingrese un número: " numero_2
+echo "$numero, $numero_1, $numero_2"
