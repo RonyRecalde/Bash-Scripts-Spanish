14c4936f01d8650dfa25fadb17545b864c0da72b
Carlos Rony Recalde <49035642+RonyRecalde@users.noreply.github.com>
2023-11-02 11:40:44 -0300

    Update interactuando-con-variables.sh

--- Scripts para Iniciantes/interactuando-con-variables.sh
+++ Scripts para Iniciantes/interactuando-con-variables.sh
@@ -2,11 +2,12 @@
 
 clear
 
-#Leyendo un argumento ingresado por teclado con el comando read
 # La opción -n permite no generar una nueva línea 
 echo -n "Ingrese su nombre: "
+#Leyendo un argumento ingresado por teclado con el comando read
 read nombre
-echo -n " Hola, $nombre. Por favor, ingrese su apellido: "
-read apellido
+#Ejecutando dos comandos usando ;
+echo -n " Hola, $nombre. Por favor, ingrese su apellido: ";read apellido
+#Visualizando el contenido asignado de las variables creadas
 echo "Hola $nombre $apellido. Bienvenido!"
 
