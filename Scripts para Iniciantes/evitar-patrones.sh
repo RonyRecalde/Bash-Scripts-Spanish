438c3acdfb5ba551468ce720a6873e54dd14ae91
Carlos Rony Recalde <49035642+RonyRecalde@users.noreply.github.com>
2023-11-07 14:08:27 -0300

    Update evitar-patrones.sh

--- Scripts para Iniciantes/evitar-patrones.sh
+++ Scripts para Iniciantes/evitar-patrones.sh
@@ -2,7 +2,8 @@
 
 clear
 
+#Definir una variable con un patrón
+letras="[aeiou]"
 read -p "Ingrese su texto: " texto
-read -p "Ingrese el patrón a evitar en su texto: " letras
 #Usamos // para evitar un patrón en un texto
 echo "Su nuevo texto es: " ${texto//letras}"
