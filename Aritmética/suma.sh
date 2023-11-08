6d9213f8e472d5821ff14d620eea4837063d999f
Carlos Rony Recalde <49035642+RonyRecalde@users.noreply.github.com>
2023-11-08 20:32:05 -0300

    Update suma.sh

--- Aritmética/suma.sh
+++ Aritmética/suma.sh
@@ -2,4 +2,5 @@
 
 read -p "Ingrese el primer número: " num1
 read -p "Ingrese el segundo número: " num2
+#Usamos el operador [] para realizar la operación
 echo "La suma de sus números es: $[num1 + num2]"
