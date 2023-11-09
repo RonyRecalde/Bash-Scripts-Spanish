7dff676409032dda829f8b59d0285a61ea85b099
Carlos Rony Recalde <49035642+RonyRecalde@users.noreply.github.com>
2023-11-09 13:27:17 -0300

    Create multiplicacion.sh

---
+++ Aritmética/multiplicacion.sh
@@ -0,0 +1,7 @@
+#!usr/bin/env bash
+
+read -p "Ingrese su primer número: " num1
+read -p "Ingrese su segundo número: " num2
+#Usamos el comando bc para realizar la operación permitiendo valores decimales, la opción scale es para determinar el número de decimales permitidos
+resultado=$(bc <<< "scale=2; $num1 * $num2")
+echo "El resultado de la multuplicación de $num1 y $num2 es: $resultado
