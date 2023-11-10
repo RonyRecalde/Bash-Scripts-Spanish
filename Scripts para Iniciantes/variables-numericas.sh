54accb4f99a92a90943c7aed3ab069b4415a4e21
Carlos Rony Recalde <49035642+RonyRecalde@users.noreply.github.com>
2023-11-10 16:35:08 -0300

    Update variables-numericas.sh

--- Scripts para Iniciantes/variables-numericas.sh
+++ Scripts para Iniciantes/variables-numericas.sh
@@ -4,7 +4,7 @@ clear
 
 #Las variables numéricas no necesitan ser colocados entre " " 
 numero=2023
-numero_1=2022
 #También se pueden ingresar valores decimales
-read -p "Ingrese un número: " numero_2
+read -p "Ingrese un primer número: " numero_1
+read -p "Ingrese un nsegundo úmero: " numero_2
 echo "Los números informados fueron: $numero, $numero_1, $numero_2"
