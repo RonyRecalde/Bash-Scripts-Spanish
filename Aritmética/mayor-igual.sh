267556eb4ff6a4e8b2004208aa9dea19f079b3d6
Carlos Rony Recalde <49035642+RonyRecalde@users.noreply.github.com>
2023-11-13 09:59:46 -0300

    Create mayor-igual.sh

---
+++ Aritmética/mayor-igual.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+clear
+
+read -p "Ingrese el primer número: " num1
+read -p "Ingrese el segundo número: " num2
+#-lt: less than
+#Usamos [[]] para realizar la operación de -ge 
+if [[ $num1 -ge $num2 ]]
+then
+  echo $num1 es mayor o igual que $num2
+else
+  echo $num2 es mayor o igual que $num1
+fi
