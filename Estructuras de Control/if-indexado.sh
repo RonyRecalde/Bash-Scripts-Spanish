5533a542db5e0b7a04ac974ac03b3d05048b46df
Carlos Rony Recalde <49035642+RonyRecalde@users.noreply.github.com>
2023-11-14 17:18:06 -0300

    Update if-indexado.sh

--- Estructuras de Control/if-indexado.sh
+++ Estructuras de Control/if-indexado.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
-num3=" "
+#clear
+
 read -p "Ingrese su primer número: " num1
 read -p "Ingrese su segundo número: " num2
-
-
+#
+#
 if [[ $num1 -eq $num2 ]]; then
         echo $num1 es igual que $num2
 elif [[ $num1 != $num2 ]]; then
-        echo $num2 no es igual que $num1
+        echo $num1 no es igual que $num2
 fi
-if [[ -z $num3 ]]; then
-  echo $num3 está vácio! Ingrese un número
+#
+if [[ $num1 -lt $num2 ]]; then
+        echo $num1 es menor que $num2
+fi
 
