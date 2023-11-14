27866453b569d104b59db6ae24008974b74c5901
Carlos Rony Recalde <49035642+RonyRecalde@users.noreply.github.com>
2023-11-14 17:19:07 -0300

    Update if-indexado.sh

--- Estructuras de Control/if-indexado.sh
+++ Estructuras de Control/if-indexado.sh
@@ -11,7 +11,7 @@ if [[ $num1 -eq $num2 ]]; then
 elif [[ $num1 != $num2 ]]; then
         echo $num1 no es igual que $num2
 fi
-#
+#Se ejecutará a continuación del else/if
 if [[ $num1 -lt $num2 ]]; then
         echo $num1 es menor que $num2
 fi
