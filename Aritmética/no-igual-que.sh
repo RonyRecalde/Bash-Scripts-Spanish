1866c62f7935ee239fa30d750b7a08fba0d4de28
Rony Recalde <rony_recalde@hotmail.com>
2023-11-23 12:26:46 -0300

    Create no-igual-que.sh

---
+++ Aritmética/no-igual-que.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+clear
+
+read -p "Ingrese el primer número: " $num1
+read -p "Ingrese el segundo número: " $num2
+#-ne: No equal than
+#Usamos el comando [[ ... ]] para validar el comparador númerico -ne
+
+if [[ $num1 -eq $num2 ]]; then
+	echo 'El número' $num1  'es diferente de' $num2
+fi
