40d5c9444b0c6dd2dfd3f11fc6936484cae6baf3
Rony Recalde <rony_recalde@hotmail.com>
2023-11-23 13:49:37 -0300

    Update while.sh

--- Estructuras de Control/while.sh
+++ Estructuras de Control/while.sh
@@ -1,2 +1,10 @@
 #!/usr/bin/env bash
 
+clear
+#Con while podemos ejecutar otros comando durante  ciclos definidos.
+#En cuanto i sea menor o igual a 5, se va a imprimir el mensaje Hola mundo.
+i=1
+while [[ $i -le 5 ]]; do
+	echo "Hola Mundo"	
+	i=$(( $i + 1 ))
+done
